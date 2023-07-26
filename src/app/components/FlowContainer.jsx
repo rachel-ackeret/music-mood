04b11d210016756a24c7f1f9111d9b6cc1b357b8
Rachel Ackeret <rachelbatesnj@gmail.com>
2023-07-26 14:34:38 -0400

    fix issue with old results component

--- src/app/components/FlowContainer.jsx
+++ src/app/components/FlowContainer.jsx
@@ -51,41 +51,39 @@ const FlowContainer = ({ genreRecommendations }) => {
 	const stringifyChoice = JSON.stringify(savedUserChoices)
 
 	return (
-		currentPlacement === moodFlows.RESULTS ?
-			<Results recommendedTracks={recommendedTracks} /> :
-			<div className="bg-zinc-800 min-h-[150px] rounded-lg block drop-shadow-lg p-9 pb-2 flex flex-col w-[650px]">
-				<div className="flow-content-container text-center">
-					<h2 className="text-2xl font-bold mb-5">{placementContent?.title}</h2>
-					<p className="mb-5">{placementContent?.description}</p>
-					{content}
-				</div>
-				<div className="flex justify-between mt-5">
-					{queuePosition > 0 && 
-						<button 
-							className="border-solid rounded-xl border-2 border-yellow-500 my-3 h-[36px] w-[150px] hover:bg-yellow-500"
-							onClick={onBack}
-						>
-							Back
-						</button>
-					}
-					{(queuePosition < moodFlowPhases.size - 2) ? (
-						<button 
-							className="border-solid rounded-xl border-2 border-green-500 my-3 h-[36px] w-[150px] hover:bg-green-500 mr-0 ml-auto"
-							onClick={onNext}
-						>
-							{placementContent.buttonText}
-						</button>
-					) : (
-						<Link href={{
-							pathname: "/your-results/",
-							query: stringifyChoice
-						}}>
-							Go to New Page
-						</Link>
-					)
-					}
-				</div>
+		<div className="bg-zinc-800 min-h-[150px] rounded-lg block drop-shadow-lg p-9 pb-2 flex flex-col w-[650px]">
+			<div className="flow-content-container text-center">
+				<h2 className="text-2xl font-bold mb-5">{placementContent?.title}</h2>
+				<p className="mb-5">{placementContent?.description}</p>
+				{content}
 			</div>
+			<div className="flex justify-between mt-5">
+				{queuePosition > 0 && 
+					<button 
+						className="border-solid rounded-xl border-2 border-yellow-500 my-3 h-[36px] w-[150px] hover:bg-yellow-500"
+						onClick={onBack}
+					>
+						Back
+					</button>
+				}
+				{(queuePosition < moodFlowPhases.size - 2) ? (
+					<button 
+						className="border-solid rounded-xl border-2 border-green-500 my-3 h-[36px] w-[150px] hover:bg-green-500 mr-0 ml-auto"
+						onClick={onNext}
+					>
+						{placementContent.buttonText}
+					</button>
+				) : (
+					<Link href={{
+						pathname: "/your-results/",
+						query: stringifyChoice
+					}}>
+						Go to New Page
+					</Link>
+				)
+				}
+			</div>
+		</div>
 	);
 };
 
